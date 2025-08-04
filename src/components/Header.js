afc4fd786c3602ba12e5fd538e5e99ad799f29ce
Abhis1619 <pabhi9018@gmail.com>
2025-08-04 16:53:46 +0530

    header.js updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,122 @@
+// import React, { useState } from 'react';
+// import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+
+
+// const Header = () => {
+//   const [isNavOpen, setIsNavOpen] = useState(false);
+//   const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+//   const toggleNav = () => {
+//     setIsNavOpen(!isNavOpen);
+//   };
+
+//   const toggleSearch = () => {
+//     setIsSearchOpen(!isSearchOpen);
+//   };
+
+//   return (
+//     <header className="header">
+//       {/* Top header */}
+//       <div className="header-top">
+//         <div className="container">
+//           <div className="header-top-wrapper">
+//             <div className="header-top-left">
+//               <div className="header-top-contact ">
+//                 <ul className='ulll '>
+//                 <li className=''>
+//                   <a href="tel:+918177869909">
+//   <i className="fas fa-phone-volume"></i> +91 8177869909
+// </a>
+//                   </li>
+//                   <li className=''>
+//                     <a href="mailto:booking@vedanttravels.in">
+//                       <i className="far fa-envelope"></i>
+//                       <span> booking@vedanttravels.in</span>
+//                     </a>
+//                   </li>
+                 
+                
+//                 </ul>
+//               </div>
+//             </div>
+           
+//           </div>
+//         </div>
+//       </div>
+
+//       {/* Main Navigation */}
+//       <div className="main-navigation">
+//         <Navbar expand="lg">
+//           <Container>
+//             <Navbar.Brand href="">
+//               <img src="./images/logo.jpg" alt="logo" />
+//             </Navbar.Brand>
+
+//             <Navbar.Toggle aria-controls="main_nav" onClick={toggleNav}>
+//               <i className="fas fa-bars darkcolor"></i> {/* Mobile Menu Icon */}
+//             </Navbar.Toggle>
+
+//             <Navbar.Collapse id="main_nav" className={isNavOpen ? "show" : ""}>
+//               <Nav className="mr-auto">
+//                 <Nav.Link href="/" className='fw-bold nav-item'>Home</Nav.Link>
+
+//                 <NavDropdown title="About " id="about-us-dropdown " className='fw-bold'>
+//                   <NavDropdown.Item href="about" className='fw-bold nav-item'>About Us</NavDropdown.Item>
+//                   <NavDropdown.Item href="/term-condition" className='fw-bold nav-item'>Term & Condition </NavDropdown.Item>
+//                   <NavDropdown.Item href="/privacy-policy" className='fw-bold '>Privacy & Policy </NavDropdown.Item>
+               
+//                 </NavDropdown>
+
+//                 <Nav.Link href="/our-fleet"  className='fw-bold nav-item'>Our Fleet</Nav.Link>
+//                 <Nav.Link href="/packages" className='fw-bold nav-item'>Packages</Nav.Link>
+
+//                 <NavDropdown title="Pune &#8652; Mumbai Cab" id="contact-dropdown" className='fw-bold'>
+//                   <NavDropdown.Item href="/pune-mumbai-cabs" className='fw-bold nav-item'>Pune &#8652; Mumbai Cab</NavDropdown.Item>
+//                   <NavDropdown.Item href="/Pune-to-Mumbai-Cab" className='fw-bold'>Pune to Mumbai Cab</NavDropdown.Item>
+//                 </NavDropdown>
+             
+
+//                 <NavDropdown title="Mumbai &#8652; Pune Cab" id="contact-dropdown" className='fw-bold'>
+//                   <NavDropdown.Item href="/Mumbai-to-Pune-Daily-Cab" className='fw-bold '>Mumbai to Pune  Cab </NavDropdown.Item>
+                  
+//                 </NavDropdown>
+
+//                 <Nav.Link href="/booking" className='fw-bold nav-item'>Booking</Nav.Link>
+//                 <Nav.Link href="/gallery" className='fw-bold nav-item'>Gallery</Nav.Link>
+
+//                 <NavDropdown title="Contact" id="contact-dropdown" className='fw-bold'>
+//                   <NavDropdown.Item href="/contact" className='fw-bold nav-item'>Contact</NavDropdown.Item>
+//                   <NavDropdown.Item href="/Enquiry" className='fw-bold '>Enquiry</NavDropdown.Item>
+//                 </NavDropdown>
+//               </Nav>
+
+              
+//             </Navbar.Collapse>
+//           </Container>
+//         </Navbar>
+//       </div>
+
+//       {/* Search Area */}
+//       {isSearchOpen && (
+//         <div className="search-area">
+//           <form action="#">
+//             <div className="form-group">
+//               <input type="text" className="form-control" placeholder="Type Keyword..." />
+//               <button type="submit" className="search-icon-btn">
+//                 <i className="far fa-search"></i>
+//               </button>
+//             </div>
+//           </form>
+//         </div>
+//       )}
+//     </header>
+//   );
+// };
+
+// export default Header;
 import React, { useState } from 'react';
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
-
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -21,25 +137,22 @@ const Header = () => {
         <div className="container">
           <div className="header-top-wrapper">
             <div className="header-top-left">
-              <div className="header-top-contact ">
-                <ul className='ulll '>
-                <li className=''>
-                  <a href="tel:+918177869909">
-  <i className="fas fa-phone-volume"></i> +91 8177869909
-</a>
+              <div className="header-top-contact">
+                <ul className="ulll">
+                  <li>
+                    <a href="tel:+918177869909">
+                      <i className="fas fa-phone-volume"></i> +91 8177869909
+                    </a>
                   </li>
-                  <li className=''>
+                  <li>
                     <a href="mailto:booking@vedanttravels.in">
                       <i className="far fa-envelope"></i>
                       <span> booking@vedanttravels.in</span>
                     </a>
                   </li>
-                 
-                
                 </ul>
               </div>
             </div>
-           
           </div>
         </div>
       </div>
@@ -48,49 +161,44 @@ const Header = () => {
       <div className="main-navigation">
         <Navbar expand="lg">
           <Container>
-            <Navbar.Brand href="">
+            <Navbar.Brand as={Link} to="/">
               <img src="./images/logo.jpg" alt="logo" />
             </Navbar.Brand>
 
             <Navbar.Toggle aria-controls="main_nav" onClick={toggleNav}>
-              <i className="fas fa-bars darkcolor"></i> {/* Mobile Menu Icon */}
+              <i className="fas fa-bars darkcolor"></i>
             </Navbar.Toggle>
 
-            <Navbar.Collapse id="main_nav" className={isNavOpen ? "show" : ""}>
+            <Navbar.Collapse id="main_nav" className={isNavOpen ? 'show' : ''}>
               <Nav className="mr-auto">
-                <Nav.Link href="/" className='fw-bold nav-item'>Home</Nav.Link>
+                <Nav.Link as={Link} to="/" className="fw-bold nav-item">Home</Nav.Link>
 
-                <NavDropdown title="About " id="about-us-dropdown " className='fw-bold'>
-                  <NavDropdown.Item href="about" className='fw-bold nav-item'>About Us</NavDropdown.Item>
-                  <NavDropdown.Item href="/term-condition" className='fw-bold nav-item'>Term & Condition </NavDropdown.Item>
-                  <NavDropdown.Item href="/privacy-policy" className='fw-bold '>Privacy & Policy </NavDropdown.Item>
-               
+                <NavDropdown title="About" id="about-us-dropdown" className="fw-bold">
+                  <NavDropdown.Item as={Link} to="/about" className="fw-bold nav-item">About Us</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/term-condition" className="fw-bold nav-item">Term & Condition</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/privacy-policy" className="fw-bold">Privacy & Policy</NavDropdown.Item>
                 </NavDropdown>
 
-                <Nav.Link href="/our-fleet"  className='fw-bold nav-item'>Our Fleet</Nav.Link>
-                <Nav.Link href="/packages" className='fw-bold nav-item'>Packages</Nav.Link>
+                <Nav.Link as={Link} to="/our-fleet" className="fw-bold nav-item">Our Fleet</Nav.Link>
+                <Nav.Link as={Link} to="/packages" className="fw-bold nav-item">Packages</Nav.Link>
 
-                <NavDropdown title="Pune &#8652; Mumbai Cab" id="contact-dropdown" className='fw-bold'>
-                  <NavDropdown.Item href="/pune-mumbai-cabs" className='fw-bold nav-item'>Pune &#8652; Mumbai Cab</NavDropdown.Item>
-                  <NavDropdown.Item href="/Pune-to-Mumbai-Cab" className='fw-bold'>Pune to Mumbai Cab</NavDropdown.Item>
-                </NavDropdown>
-             
-
-                <NavDropdown title="Mumbai &#8652; Pune Cab" id="contact-dropdown" className='fw-bold'>
-                  <NavDropdown.Item href="/Mumbai-to-Pune-Daily-Cab" className='fw-bold '>Mumbai to Pune  Cab </NavDropdown.Item>
-                  
+                <NavDropdown title="Pune ↔ Mumbai Cab" id="pune-mumbai-cab" className="fw-bold">
+                  <NavDropdown.Item as={Link} to="/pune-mumbai-cabs" className="fw-bold nav-item">Pune ↔ Mumbai Cab</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/Pune-to-Mumbai-Cab" className="fw-bold">Pune to Mumbai Cab</NavDropdown.Item>
                 </NavDropdown>
 
-                <Nav.Link href="/booking" className='fw-bold nav-item'>Booking</Nav.Link>
-                <Nav.Link href="/gallery" className='fw-bold nav-item'>Gallery</Nav.Link>
+                <NavDropdown title="Mumbai ↔ Pune Cab" id="mumbai-pune-cab" className="fw-bold">
+                  <NavDropdown.Item as={Link} to="/Mumbai-to-Pune-Daily-Cab" className="fw-bold">Mumbai to Pune Cab</NavDropdown.Item>
+                </NavDropdown>
 
-                <NavDropdown title="Contact" id="contact-dropdown" className='fw-bold'>
-                  <NavDropdown.Item href="/contact" className='fw-bold nav-item'>Contact</NavDropdown.Item>
-                  <NavDropdown.Item href="/Enquiry" className='fw-bold '>Enquiry</NavDropdown.Item>
+                <Nav.Link as={Link} to="/booking" className="fw-bold nav-item">Booking</Nav.Link>
+                <Nav.Link as={Link} to="/gallery" className="fw-bold nav-item">Gallery</Nav.Link>
+
+                <NavDropdown title="Contact" id="contact-dropdown" className="fw-bold">
+                  <NavDropdown.Item as={Link} to="/contact" className="fw-bold nav-item">Contact</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/enquiry" className="fw-bold">Enquiry</NavDropdown.Item>
                 </NavDropdown>
               </Nav>
-
-              
             </Navbar.Collapse>
           </Container>
         </Navbar>
